554c3b98ff461684c6a11b0e473a5b464542f015
rainikita <nikita.rai@tibilsolutions.com>
2024-04-18 19:29:54 +0530

    yml file

--- Reports/send_email_script.py
+++ Reports/send_email_script.py
@@ -5,12 +5,46 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
 
+# def send_email(username, password, recipient_email):
+#     subject = "Selenium Test Execution Report"
+#     body = "Please find the attached test execution report."
+#
+#     sender_email = username
+#     receiver_email = recipient_email
+#
+#     msg = MIMEMultipart()
+#     msg['From'] = sender_email
+#     msg['To'] = receiver_email
+#     msg['Subject'] = subject
+#
+#     msg.attach(MIMEText(body, 'plain'))
+#
+#     # Attach the test report file
+#     with open('./Reports/homepage.html', 'rb') as f:
+#         attach = MIMEApplication(f.read(), _subtype="html")
+#         attach.add_header('Content-Disposition', 'attachment', filename=str("homepage.html"))
+#         msg.attach(attach)
+#
+#     # Send the email
+#     with smtplib.SMTP('smtp.gmail.com', 587) as server:
+#         server.starttls()
+#         server.login(username, password)
+#         server.sendmail(sender_email, receiver_email, msg.as_string())
+#
+#
+# if __name__ == "__main__":
+#     gmail_username = sys.argv[1]
+#     gmail_password = sys.argv[2]
+#     recipient_email = sys.argv[3]
+#
+#     send_email(gmail_username, gmail_password, recipient_email)
+
 def send_email(username, password, recipient_email):
     subject = "Selenium Test Execution Report"
     body = "Please find the attached test execution report."
 
-    sender_email = username
-    receiver_email = recipient_email
+    sender_email = username.decode('utf-8')  # Decode bytes to string
+    receiver_email = recipient_email.decode('utf-8')  # Decode bytes to string
 
     msg = MIMEMultipart()
     msg['From'] = sender_email
@@ -20,21 +54,14 @@ def send_email(username, password, recipient_email):
     msg.attach(MIMEText(body, 'plain'))
 
     # Attach the test report file
-    with open('./Reports/homepage.html', 'rb') as f:
+    with open('./Reports/report.html', 'rb') as f:
         attach = MIMEApplication(f.read(), _subtype="html")
-        attach.add_header('Content-Disposition', 'attachment', filename=str("homepage.html"))
+        attach.add_header('Content-Disposition', 'attachment', filename=str("report.html"))
         msg.attach(attach)
 
     # Send the email
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
-        server.login(username, password)
+        server.login(username.decode('utf-8'), password.decode('utf-8'))  # Decode bytes to string
         server.sendmail(sender_email, receiver_email, msg.as_string())
 
-
-if __name__ == "__main__":
-    gmail_username = sys.argv[1]
-    gmail_password = sys.argv[2]
-    recipient_email = sys.argv[3]
-
-    send_email(gmail_username, gmail_password, recipient_email)
